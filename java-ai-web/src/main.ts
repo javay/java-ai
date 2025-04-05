0e288daecf9f17299d9e362dc3a497f307d61843
张恕征 <zhangsz10244@fxiaoke.com>
2025-04-06 01:29:41 +0800

    feat: i18n use zhCn

--- java-ai-web/src/main.ts
+++ java-ai-web/src/main.ts
@@ -8,10 +8,13 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
